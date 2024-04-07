3686b7d2601017992746b9a23e203d1651a1b8b7
sMarchane17 <sif-din.marchane.1@ens.etsmtl.ca>
2024-04-07 12:56:27 -0400

    Test3

--- Application/Main.java
+++ Application/Main.java
@@ -2,6 +2,7 @@ package Application;
 
 import javax.swing.SwingUtilities;
 
+//Test3
 public class Main {
     public static void main(String[] args) {
         Controller controller = new Controller();
